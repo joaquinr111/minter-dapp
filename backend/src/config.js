933c6cee78df24e414a253257de52106b1b697be
joaquinr111 <98616915+joaquinr111@users.noreply.github.com>
2022-03-21 17:39:30 -0400

    commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -36,7 +36,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "https://indiecats.club"; // Replace with your website or remove this line if you do not have one.
+  external_url: "https://indiecats.club", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
